813af254278afc1c6c660cd24ac9579d93cf6f4c
Sebastian te Brake <63264999+teeebrake@users.noreply.github.com>
2022-03-04 13:46:36 +0100

    updated api request

--- streamlit.py
+++ streamlit.py
@@ -5,19 +5,43 @@ import seaborn as sns
 import streamlit as st
 import matplotlib.pyplot as plt
 import plotly.express as px
+import requests
+
+#connect to tankerkoenig.de api and retrieve live infos
+
+api_key = 'dc0d5dcb-4386-6f2a-74df-83221c34fd55'
+
+headers = {'Authorization': 'Bearer {}'.format(api_key)}
+search_api_url = 'https://creativecommons.tankerkoenig.de/json/prices.php?'
+params = {'ids': '813ed58c-b58d-4d17-895b-2078cb302649',
+        'apikey': 'dc0d5dcb-4386-6f2a-74df-83221c34fd55',
+          }
+
+response = requests.get(search_api_url, headers=headers, params=params, timeout=5)
+
+print(response.url)
+print(response.status_code)
+print(response.headers)
+
+data_dict = response.json()
 
 # Write a title
 st.write('Gas Price Explorer')
 # Write data taken from https://allisonhorst.github.io/palmerpenguins/
-st.write('Little app for exploring [Aral Tankstelle	ARAL	Prinzenstraße	29] (http://wikimapia.org/11225557/de/Aral-Tankstelle-Prinzenstraße-29)')
+st.write('Little webapp for exploring [Aral Tankstelle	ARAL	Prinzenstraße	29] (http://i4.wikimapia.org/?x=140836&y=85993&zoom=18&type=map&lng=0)')
 
+#http://wikimapia.org/11225557/de/Aral-Tankstelle-Prinzenstraße-29
 # Put image
 st.image('https://cdn.images.express.co.uk/img/dynamic/24/750x445/1075641.jpg')
+
+st.write ('Current prices at Ritterstraße', data_dict['prices'])
+
 # Write heading for Data
 st.header('Data')
 # Read csv file and output a sample of 20 data points
 df_gas_price = pd.read_csv('ritter_all_14_22.csv', sep=',')
-st.write('Display a sample of data points from `tankerkoenig.de`', df_gas_price.sample(20))
+st.write('Display a sample of data points from `tankerkoenig.de`', df_gas_price)
+#st.write('Display a sample of data points from `tankerkoenig.de`', df_gas_price.sample(20))
 # Add a selectbox for type of fuel/gas
 
 gas_type = st.selectbox('Choose a type of gas',['diesel','e5','e10'])
